3d74b2ee64553839556961fe546e167ec8c29b5e
Francisco Italo <italo.silvan87@gmail.com>
2023-10-03 20:01:56 -0300

    3-feature: adicionar atualizações

---
+++ src/Page/Landing/components/Content/Content.tsx
@@ -0,0 +1,21 @@
+import { Grid, Typography } from "@mui/joy";
+import Saly from "../../../../assets/Saly-22.png";
+import Onda from "../../../../assets/onda.svg";
+import style from "./Content.module.css";
+
+export const Content = () => {
+  return (
+    <Grid className={style.containerGrid}>
+      <div>
+        <Typography className={style.title}>
+          Receitas Médicas Simples e Seguras
+        </Typography>
+      </div>
+      <div className={style.containerImgs}>
+        <img src={Onda} className={style.imgOnda} />
+
+        <img src={Saly} className={style.imgSally} />
+      </div>
+    </Grid>
+  );
+};
