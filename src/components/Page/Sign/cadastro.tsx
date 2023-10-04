a00092d482c9f738535ca1796c81c6d4ec476c6c
unknown <nandagb23@gmail.com>
2023-10-03 22:13:41 -0300

    adicionando telas estaticas

---
+++ src/components/Page/Sign/cadastro.tsx
@@ -0,0 +1,74 @@
+import { useState } from 'react'
+import styles from './SignUp.module.css';
+import Input from '@mui/joy/Input';
+import Card from '@mui/joy/Card';
+
+
+
+
+function SignUp() {
+    const [formData, setFormData] = useState({
+      email: '',
+      senha: '',
+    });
+  
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const { name, value } = e.target;
+      setFormData({
+        ...formData,
+        [name]: value,
+      });
+    };
+  
+    const handleSubmit = (e: React.FormEvent) => {
+      e.preventDefault();
+      // Lógica para enviar os dados do formulário para o servidor ou realizar outras ações
+    };
+  
+    return (
+      
+        <Card variant="solid" color="primary" invertedColors sx={{ maxWidth: 343}} className={styles['signup-container']}>
+          <h2 className={styles['signup-title']}>Criar conta</h2>
+          <form onSubmit={handleSubmit}>
+            <div className={styles['input-field']}>
+              <label htmlFor="email" className={styles['input-label']}>
+                Email:
+              </label>
+              <Input
+                size="sm"
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                className={styles['input']}
+                variant="soft"
+              />
+            </div>
+            <div className={styles['input-field']}>
+              <label htmlFor="senha" className={styles['input-label']}>
+                Senha:
+              </label>
+              <Input
+                size="sm"
+                type="password"
+                id="senha"
+                name="senha"
+                value={formData.senha}
+                onChange={handleChange}
+                className={styles['input']}
+                variant="soft"
+              />
+            </div>
+            <div className={styles['button-container']}>
+              <button className={styles['button']} type="submit">
+                Criar conta
+              </button>
+              <button className={styles['button']}>Já tenho uma conta</button>
+            </div>
+          </form>
+        </Card>
+    );
+  }
+  
+  export default SignUp;
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Landing } from "./components/Page/Landing/index.tsx";
+import SignUp from "./components/Page/Sign/cadastro.tsx";
+import SignIn from "./components/Page/Sign/login.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Landing />,
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
+  },
+  {
+    path: "/signin",
+    element: <SignIn />,
   },
 ]);
 ReactDOM.createRoot(document.getElementById("root")!).render(
