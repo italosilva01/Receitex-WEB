33fdc9800c9fa0ab8035a3fb4d78161be00cccb8
michelle <michelleteixeieramartins@gmail.com>
2023-10-05 09:06:45 -0300

    add pagina para reistro de receita

---
+++ src/components/HeaderCreateAccount/HeaderDefault.tsx
@@ -0,0 +1,13 @@
+import { Grid, Button } from "@mui/joy";
+import { Logo } from "../Logo/Logo";
+import styles from "./HeaderCreateAccount.module.css";
+
+export const HeaderDefault = () => {
+  return (
+    <Grid container className={styles.container}>
+      <Grid item alignItems="center" className={styles.content}>
+        <Logo />
+      </Grid>
+    </Grid>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import { Landing } from "./Page/Landing/index.tsx";
+import { Register } from "./Page/register/index.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Landing />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
   },
 ]);
 ReactDOM.createRoot(document.getElementById("root")!).render(
