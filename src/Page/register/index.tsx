d8329b100785cc05c712482d5eb05680613826a3
unknown <nandagb23@gmail.com>
2023-10-26 22:12:46 -0300

    adicionando campos para a tela de criar receita

--- src/Page/register/index.tsx
+++ src/Page/register/index.tsx
@@ -1,6 +1,8 @@
 import { Grid, Button, ButtonGroup, Box, IconButton } from "@mui/joy";
 import Sheet from '@mui/joy/Sheet';
 import Textarea from '@mui/joy/Textarea';
+import Select from '@mui/joy/Select';
+import Option from '@mui/joy/Option';
 import Typography from '@mui/joy/Typography';
 import {FormatItalic, FormatBold, FormatUnderlined} from '@mui/icons-material';
 import { useState } from 'react';
@@ -10,6 +12,18 @@ import { HeaderDefault } from "../../components/HeaderCreateAccount/HeaderDefaul
 export const Register = () => {
   const [titulo, setTitulo] = useState('');
   const [descricao, setDescricao] = useState('');
+  const [tipoDocumento, setTipoDocumento] = useState('Receita');;
+
+  const handleDocumentSelection = (event) => {
+    setTipoDocumento(event.target.value);
+  }
+
+  const handleVencimento = (event) => {
+    
+  }
+
+  
+  
 
   const handleSave = async () => {
 
@@ -53,9 +67,10 @@ export const Register = () => {
       <Typography level="h3" sx={{ml: "20rem", mt:"1rem", color: '#ffffff'}}>Crie sua receita para</Typography>
       <Typography sx={{ml: "20rem", color: '#ffffff'}}>Rodolfo</Typography>
       </Sheet>
-      <Grid ml="13rem">
-          <Box  style={{ width: '5%', borderRadius: '6px', padding: '10px', marginBottom: '10px', marginTop: '40px' }}
-          sx={{ p: 1, backgroundColor: 'blue', borderRadius: '6px', color: 'white', textAlign: 'center' }}>
+      <Grid container direction="row">
+        <Grid ml="13rem">
+          <Box style={{ width: '80%', borderRadius: '6px', padding: '10px', marginBottom: '10px', marginTop: '40px'}}
+            sx={{ p: 1, backgroundColor: 'blue', borderRadius: '6px', color: 'white', textAlign: 'center' }}>
             Título
           </Box>
           <input
@@ -63,9 +78,46 @@ export const Register = () => {
             value={titulo}
             onChange={(e) => setTitulo(e.target.value)}
             placeholder="Digite o título aqui"
-            style={{ width: '20%', borderRadius: '6px', padding: '10px', marginBottom: '10px' }}
+            style={{ width: '80%', borderRadius: '6px', padding: '10px', marginBottom: '10px' }}
           />
         </Grid>
+
+        <Grid ml="5em">
+          <Box color="primary" style={{ width: '100%', borderRadius: '6px', padding: '10px', marginBottom: '10px', marginTop: '40px' }}
+            sx={{ p: 1, backgroundColor: 'blue', borderRadius: '6px', color: 'white', textAlign: 'center' }}>
+            Tipo de Documento
+          </Box>
+          <Select defaultValue="receita" onChange={handleDocumentSelection}>
+            <Option value="receita">Receita</Option>
+            <Option value="requisicao">Requisição</Option>
+            <Option value="atestado">Atestado</Option>  
+          </Select>
+        </Grid>
+
+        <Grid ml="7em">
+          <Box style={{ width: '80%', borderRadius: '6px', padding: '10px', marginBottom: '10px', marginTop: '40px' }}
+            sx={{ p: 1, backgroundColor: 'blue', borderRadius: '6px', color: 'white', textAlign: 'center' }}>
+            Vencimento
+          </Box>
+          <Grid container direction="row">
+            <Grid>
+              <input
+                type="text"
+                value={titulo}
+                onChange={(e) => setTitulo(e.target.value)}
+                placeholder="quantidade"
+                style={{ width: '50%', borderRadius: '6px', padding: '10px', marginBottom: '10px' }}
+              />
+            </Grid>
+            <Select defaultValue="dias" onChange={handleVencimento}>
+              <Option value="dias">Dias</Option>
+              <Option value="meses">Meses</Option>
+              <Option value="anos">Anos</Option>  
+            </Select>
+          </Grid>
+        </Grid>
+      </Grid>
+
       <Grid container direction="row" justifyContent="space-between" alignItems="center" mt={10} mb={10}>
         <Grid ml="13rem">
             <Box sx={{ p: 1, backgroundColor: '#f9f9f9', borderRadius: '10px'}}>
